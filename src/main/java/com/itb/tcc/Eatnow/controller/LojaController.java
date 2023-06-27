077fcc7f48f03ccab94bb94fccd8ec500f26b864
carvalhovinni <carlosvivi963@gmail.com>
2023-06-27 08:28:14 -0300

    Edição do template produtos e preparação para listar os produtos

--- src/main/java/com/itb/tcc/Eatnow/controller/LojaController.java
+++ src/main/java/com/itb/tcc/Eatnow/controller/LojaController.java
@@ -1,16 +1,46 @@
 package com.itb.tcc.Eatnow.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.itb.tcc.Eatnow.model.Produto;
 
 @Controller
 @RequestMapping("/comercio/produtos")
 public class LojaController {
 	
 	@GetMapping("/listar")
-	public String listarprodutos() {
-		System.out.println("Lista de Produtos");
+	public String listarprodutos(Model model) {
+       
+		List<Produto> listaDeProdutos = new ArrayList<Produto>();
+		
+		Produto p1 = new Produto();
+		
+		p1.setId(20l);
+		p1.setNome("Máquina de lavar");
+		p1.setDescricao("Máquina de lavar Brastemp 13l com painel digital");
+		p1.setCodigoBarra("HFOEHFHERHK1446464741");
+		p1.setPreco(3215.65);
+		
+		Produto p2 = new Produto();
+		
+		p1.setId(20l);
+		p1.setNome("Micro-ondas");
+		p1.setDescricao("Micro-ondas Eletrolux multiuso");
+		p1.setCodigoBarra("DJFAFENAOENA646484548");
+		p1.setPreco(3215.65);
+		
+		// Guardando os produtos no array
+		listaDeProdutos.add(p1);
+		listaDeProdutos.add(p2);
+		
+		// Passando a lista de produtos para o front-end
+		model.addAttribute("ListaDeProdutos", listaDeProdutos);
+		
 		return "produtos";
 	}
 
--- src/main/java/com/itb/tcc/Eatnow/model/Produto.java
+++ src/main/java/com/itb/tcc/Eatnow/model/Produto.java
@@ -9,11 +9,12 @@ public class Produto {
 	private double preco;
 	
 	// public: Acesso liberado para todas as classes.
-	// private: Acesso permitido apenas para od membros
+	// private: Acesso permitido apenas para os membros
 	// da própria classe.
 	// Entende-se por membros (atributos e ou métodos)
 	
-	// Métodos Setter's e Getter
+	// Métodos Setter's e Getter são responsáveis em atribuir e recuperar informações
+	// do atributos "RESPECTIVAMENTE"
 	
 	public void setId(Long id) {
 		this.id = id;
@@ -22,4 +23,39 @@ public class Produto {
 	public Long getId() {
 		return id;
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getCodigoBarra() {
+		return codigoBarra;
+	}
+
+	public void setCodigoBarra(String codigoBarra) {
+		this.codigoBarra = codigoBarra;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
+	
 }
